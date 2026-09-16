23ebd871245816e6b990d4fd6d4db990817efc8c
Nguyễn Trần Anh Hoàng <hoangvb11xs@gmail.com>
2026-09-16 10:22:44 +0700

    chore(compliance): add build-from-source docs, fix remaining license headers

--- ai-models/ollama_setup.sh
+++ ai-models/ollama_setup.sh
@@ -1,1 +1,4 @@
+# Copyright (c) 2026 Nguyen Tran Anh Hoang
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 # Script tự động tải model AI (VD: llama3:8b) về máy local
